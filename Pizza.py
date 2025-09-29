cb34699eebbeaf6749f079f6d268c29ba8965a6d
leonidseoshnik <leonseoprofi@gmail.com>
2025-09-29 19:29:00 +0300

    add Pizza.py

---
+++ Pizza.py
@@ -0,0 +1,37 @@
+"""Pizza
+Now the pizzeria wants to implement a nice string representation for the Pizza and Ingredient classes.
+The string representation of the Ingredient class should consist of the ingredient's name and its amount in grams.
+The string representation of the Pizza class should start with the phrase:
+Pizza <pizza_name> consists of:
+followed by each ingredient listed on a separate line, ordered by descending amount (in grams).
+"""
+
+
+class Ingredient:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def __str__(self):
+        return f'{self.name}: {self.weight}г.'
+
+    def __repr__(self):
+        return f'{self.name}: {self.weight}'
+
+
+class Pizza:
+    def __init__(self, name, ingredients=None):
+        self.name = name
+        if ingredients is None:
+            self.ingredients = []
+        else:
+            self.ingredients = ingredients
+
+    def ingredient_to_txt(self, ingredients):
+        result = ''
+        for ingredient in sorted(self.ingredients, key=lambda x: -x.weight):
+            result += str(ingredient) + '\n'
+        return result
+
+    def __str__(self):
+        return f'Пицца {self.name} состоит из:\n{self.ingredient_to_txt(self.ingredients)}'
