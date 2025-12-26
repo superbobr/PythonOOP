471ca37479dfa93d04cfd86e09baf4189cb83a1a
leonidseoshnik <leonseoprofi@gmail.com>
2025-12-26 15:51:57 +0200

    add some classes

--- operator_overload.py
+++ operator_overload.py
@@ -21,3 +21,32 @@ class Person:
         if not isinstance(other, Person):
             return False
         return (self.name == other.name) and (self.age == other.age)
+
+
+class Playlist:
+    def __init__(self, title, songs):
+        self.title = title
+        self.songs = songs
+
+    def __len__(self):
+        return len(self.songs)
+
+
+class Grades:
+    def __init__(self):
+        self._grades = {"math": 5, "history": 4}
+
+    def __getitem__(self, subject):
+        return self._grades[subject]
+
+
+class Item:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def __repr__(self):
+        return f'Item({self.name!r}, {self.price!r})'
+
+    def __lt__(self, other):
+        return self.price < other.price
