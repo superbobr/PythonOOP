3949c42f8853d188ac78b4f1545b390140699cdc
leonidseoshnik <leonseoprofi@gmail.com>
2026-01-23 18:41:39 +0200

    add some classes

--- abstract.py
+++ abstract.py
@@ -13,3 +13,38 @@ class Square(Shape):
 
     def area(self):
         return self.side * self.side
+
+
+class DataSource(ABC):
+    @abstractmethod
+    def read(self):
+        pass
+
+    @abstractmethod
+    def write(self, data):
+        pass
+
+
+class FileStorage(DataSource):
+    def read(self):
+        return "Чтение из файла"
+
+    def write(self, data):
+        return f"Запись в файл: {data}"
+
+
+class Instrument(ABC):
+    def __init__(self, brand):
+        self.brand = brand
+
+    def show_brand(self):
+        return f"Бренд: {self.brand}"
+
+    @abstractmethod
+    def play(self):
+        pass
+
+
+class Guitar(Instrument):
+    def play(self):
+        return "Играет мелодия на гитаре"
