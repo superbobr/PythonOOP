fde1581b2877f311bc6b8ad44622592a11718a68
leonidseoshnik <leonseoprofi@gmail.com>
2026-01-25 17:37:25 +0200

    add ManagedAttribute class

--- descriptors.py
+++ descriptors.py
@@ -6,10 +6,21 @@ class ConstantDescriptor:
         return self.value
 
     def __set__(self, instance, value):
-        raise AttributeError("Cannot change a constant value. PI is read-only.")
+        raise AttributeError("You cannot change a constant value.")
 
 
 class MyClass:
     PI = ConstantDescriptor(3.14159)
 
 
+class ManagedAttribute:
+    def __set_name__(self, owner, name):
+        self.private_name = '_' + name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return getattr(instance, self.private_name, None)
+
+    def __set__(self, instance, value):
+        setattr(instance, self.private_name, value)
