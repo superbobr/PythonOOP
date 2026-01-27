70bb63bf3aa05f237f506abcbc267b560b618ad2
leonidseoshnik <leonseoprofi@gmail.com>
2026-01-27 19:53:10 +0200

    add ValidatedString, NonNegative and LoggedAccess classes

--- descriptors.py
+++ descriptors.py
@@ -24,3 +24,45 @@ class ManagedAttribute:
 
     def __set__(self, instance, value):
         setattr(instance, self.private_name, value)
+
+
+class ValidatedString:
+    def __set_name__(self, owner, name):
+        self.private_name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.private_name, None)
+
+    def __set__(self, instance, value):
+        if isinstance(value, str):
+            setattr(instance, self.private_name, value)
+        else:
+            raise TypeError()
+
+
+class NonNegative:
+    def __set_name__(self, owner, name):
+        self.private_name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.private_name)
+
+    def __set__(self, instance, value):
+        if isinstance(value, (int, float)) and value >= 0:
+            setattr(instance, self.private_name, value)
+        else:
+            raise ValueError()
+
+
+class LoggedAccess:
+    def __set_name__(self, owner, name):
+        self.public_name = name
+        self.private_name = '_' + name
+
+    def __get__(self, instance, owner):
+        print(f"Чтение атрибута '{self.public_name}'")
+        return getattr(instance, self.private_name, None)
+
+    def __set__(self, instance, value):
+        print(f"Запись атрибута '{self.public_name}', новое значение = {value}")
+        setattr(instance, self.private_name, value)
