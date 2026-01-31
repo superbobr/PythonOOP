1f5e64b06bc1efff8b818532da95e93fbe514947
leonidseoshnik <leonseoprofi@gmail.com>
2026-01-31 19:48:42 +0200

    add class User

--- dataclasses.py
+++ dataclasses.py
@@ -25,3 +25,19 @@ class Employee:
 class Team:
     name: str
     members: List[str]=field(default_factory=list)
+
+
+@dataclass
+class User:
+    name: str
+    age: int
+
+
+user1_name = input()
+user1_age = int(input())
+user2_name = input()
+user2_age = int(input())
+
+user1 = User(user1_name, user1_age)
+user2 = User(user2_name, user2_age)
+print(user1 == user2)
