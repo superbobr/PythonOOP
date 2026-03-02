e36dea8b256f070c8b954d092aff3cf69e969a3f
leonidseoshnik <leonseoprofi@gmail.com>
2026-03-02 20:14:39 +0200

    update files.py

--- files.py
+++ files.py
@@ -6,3 +6,18 @@ with open('numbers.txt', 'w+', encoding='utf-8') as file:
     for line in file:
         result += int(line)
     print(result)
+
+with open('data.txt', 'w+', encoding='utf-8') as file:
+    file.write('hello world' + '\n')
+    file.write('python is great')
+    file.seek(0)
+    res = file.readlines()
+    for word in res:
+        print(word.strip().upper())
+
+lines = ['Строка 1', 'Строка 2', 'Строка 3']
+with open('list_data.txt', 'w+', encoding='utf-8') as file:
+    file.writelines(f'{line}\n' for line in lines)
+    file.seek(0)
+    for line in file:
+        print(line, end='')
