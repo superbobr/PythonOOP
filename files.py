d5530686ff79123b88b98e485280165402757cea
leonidseoshnik <leonseoprofi@gmail.com>
2026-03-05 11:47:18 +0200

    update files.py

--- files.py
+++ files.py
@@ -21,3 +21,41 @@ with open('list_data.txt', 'w+', encoding='utf-8') as file:
     file.seek(0)
     for line in file:
         print(line, end='')
+
+with open('events.log', 'a+', encoding='utf-8') as file:
+    for _ in range(3):
+        file.write(input() + '\n')
+    file.seek(0)
+    for line in file:
+        print(line, end='')
+
+with open('text.txt', 'w+', encoding='utf-8') as f:
+    f.write('словам\n')
+    f.write('программирование\n')
+    f.write('строка.')
+    f.seek(0)
+    s = f.readlines()
+    s.sort(key=len)
+    print(s[-1])
+
+n = int(input())
+with open('times_table.txt', 'w+', encoding='utf-8') as file:
+    for num in range(1, 11):
+        file.write(f'{n} x {num} = {n * num}\n')
+    file.seek(0)
+    for line in file:
+        print(line, end='')
+
+with open('lines.txt', 'w+', encoding='utf-8') as file:
+    file.write("alpha\n")
+    file.write("beta\n")
+    file.write("gamma")
+    file.seek(0)
+    s = file.readlines()
+    s.reverse()
+    with open('reversed_lines.txt', 'w+', encoding='utf-8') as f:
+        for line in s:
+            f.write(line.strip() + '\n')
+        f.seek(0)
+        for line in f:
+            print(line, end='')
